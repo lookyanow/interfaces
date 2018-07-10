613802ff43664419c36a47c6e2a048b7906c5a72
Ivan Lookyanow <i.lukyanov2@mangotele.com>
2018-07-10 19:49:42 +0300

    new implemention of interfaces

--- main.go
+++ main.go
@@ -2,29 +2,35 @@ package main
 
 import "fmt"
 
-type bot interface {
-	getGreeting() string
+type shape interface {
+	getArea() float64
 }
 
-type englishBot struct{}
-type spanishBot struct{}
+type triangle struct {
+	base   float64
+	height float64
+}
+type square struct {
+	sideLength float64
+}
 
 func main() {
-	eb := englishBot{}
-	sb := spanishBot{}
+	tr := triangle{base: 3.0, height: 2.0}
+	sq := square{sideLength: 3}
 
-	printGreeting(eb)
-	printGreeting(sb)
+	printArea(tr)
+	printArea(sq)
+
 }
 
-func printGreeting(b bot) {
-	fmt.Println(b.getGreeting())
+func printArea(s shape) {
+	fmt.Println(s.getArea())
 }
 
-func (englishBot) getGreeting() string {
-	return "Hello there"
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
 }
 
-func (spanishBot) getGreeting() string {
-	return "Hola!"
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
 }
